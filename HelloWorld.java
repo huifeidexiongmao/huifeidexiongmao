7e6d55b174c03e74b080b1f654de4ba377b33be9
xiaohe <2386774150@qq.com>
2024-07-08 11:23:58 +0800

    v3.0版本

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,10 @@ public class HelloWorld{
 		System.out.println("你好 世界");
 		int[] ints = {1,2,3,4,5};
 		System.out.println(ints);
+		System.out.println(ints[0]);
+		System.out.println(ints[1]);
+		System.out.println(ints[2]);
+		System.out.println(ints[3]);
+		System.out.println(ints[4]);
 	}
 }
